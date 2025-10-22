13d7ec8db544d681095c67673885857450ca8dc9
portoarco <arcoanggoro78@gmail.com>
2025-10-22 08:36:51 +0700

    Update donate dialog

--- src/app/components/DonateDialog.tsx
+++ src/app/components/DonateDialog.tsx
@@ -6,8 +6,12 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Check, X } from "lucide-react";
-import { useState } from "react";
+import { Check, CircleX, Send, X } from "lucide-react";
+import { useEffect, useState } from "react";
+import ToolTipHover from "./ToolTipHover";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 interface IDonateDialog {
   open: boolean;
@@ -18,6 +22,10 @@ export default function DonateDialog({ open, setOpen }: IDonateDialog) {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<
     "manual" | "auto" | null
   >(null);
+
+  useEffect(() => {
+    setSelectedPaymentMethod("manual");
+  }, []);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -30,15 +38,15 @@ export default function DonateDialog({ open, setOpen }: IDonateDialog) {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Informasi Donasi</DialogTitle>
-          <DialogDescription>
+          <DialogDescription className="max-sm:hidden">
             Mohon Lengkapi Data Diri serta Metode Pembayaran yang Anda Inginkan
           </DialogDescription>
-          <section id="payment-methods" className="grid grid-cols-2 gap-2">
+          <section id="payment-methods" className="grid   grid-cols-2 gap-2">
             <button
               onClick={() => setSelectedPaymentMethod("manual")}
-              className={`relative p-2 border rounded-md transition-colors duration-200 max-sm:text-sm  ${
+              className={`relative p-2 border rounded-md transition-colors duration-200 max-sm:text-xs cursor-pointer  ${
                 selectedPaymentMethod === "manual"
-                  ? "border-teal-500 bg-teal-50 font-medium"
+                  ? "border-teal-500 bg-teal-50 "
                   : "border-gray-300 hover:border-teal-400 "
               }`}
             >
@@ -48,27 +56,84 @@ export default function DonateDialog({ open, setOpen }: IDonateDialog) {
               )}
             </button>
 
-            <button
-              disabled
-              onClick={() => setSelectedPaymentMethod("auto")}
-              className={`relative p-2 border rounded-md transition-colors duration-200 max-sm:text-sm text-gray-500 ${
-                selectedPaymentMethod === "auto"
-                  ? //   ? "border-teal-500 bg-teal-50 font-medium" //dipakai saat sudah ready
-                    //   : "border-gray-300 hover:border-teal-400"
-                    "border-red-500 bg-red-50 font-medium"
-                  : "border-gray-300 "
-              }`}
+            <ToolTipHover
+              label={
+                <div className="flex gap-2 items-center ">
+                  <CircleX className="size-4" />
+                  Metode Belum Tersedia
+                </div>
+              }
             >
-              Pembayaran Otomatis
-              {selectedPaymentMethod === "auto" && (
-                // <Check className="size-5 bg-teal-500 text-white rounded-full absolute -top-2 -right-2" />
-                <X className="size-5 bg-red-500 text-white rounded-full absolute -top-2 -right-2" />
-              )}
-            </button>
+              <button
+                disabled
+                onClick={() => setSelectedPaymentMethod("auto")}
+                className={`relative p-2 border rounded-md transition-colors duration-200 max-sm:text-xs text-gray-500 ${
+                  selectedPaymentMethod === "auto"
+                    ? //   ? "border-teal-500 bg-teal-50 font-medium" //dipakai saat sudah ready
+                      //   : "border-gray-300 hover:border-teal-400"
+                      "border-red-500 bg-red-50"
+                    : "border-gray-300 "
+                }`}
+              >
+                Pembayaran Otomatis
+                {selectedPaymentMethod === "auto" && (
+                  // <Check className="size-5 bg-teal-500 text-white rounded-full absolute -top-2 -right-2" />
+                  <X className="size-5 bg-red-500 text-white rounded-full absolute -top-2 -right-2" />
+                )}
+              </button>
+            </ToolTipHover>
           </section>
-          <section id="donatur-form">
-            <form>
-              <label>Nama</label>
+          <section
+            id="donatur-form"
+            className="grid max-sm:grid-cols-1 grid-cols-2 items-center max-sm:gap-0 gap-5 mt-5 w-full"
+          >
+            <div id="qris" className=" p-1 rounded-md  ">
+              <Image
+                src="/assets/qris.jpg"
+                alt="qris"
+                width={350}
+                height={150}
+                className="max-sm:w-40 max-sm:mx-auto "
+              />
+              <div className="flex flex-col gap-1">
+                <p className="text-sm">
+                  Rekening BRI :{" "}
+                  <span className="font-semibold">038601002953304</span>{" "}
+                </p>
+                <p className="text-xs">
+                  a.n.{" "}
+                  <span className="font-semibold">
+                    Yayasan Adhirajasa Kusuma Bakti
+                  </span>{" "}
+                </p>
+              </div>
+            </div>
+
+            <form className="flex flex-col gap-3">
+              <div className="max-sm:flex gap-2">
+                <div>
+                  <label className="max-sm:text-sm">Nama/Inisial</label>
+                  <Input className="max-sm:text-xs" placeholder="John" />
+                </div>
+                <div>
+                  <label className="max-sm:text-sm">Email</label>
+                  <Input
+                    className="max-sm:text-xs"
+                    type="email"
+                    placeholder="johndoe@mail.com"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="max-sm:text-sm">Upload Bukti</label>
+                <Input type="file" className="cursor-pointer" />
+              </div>
+              <Button
+                type="button"
+                className="bg-teal-600 hover:bg-teal-700 cursor-pointer"
+              >
+                <Send /> Konfirmasi
+              </Button>
             </form>
           </section>
         </DialogHeader>
